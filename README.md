89a9d4a5ed804b580ce9a13185a145fd2b96a639
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-05-04 13:49:58 +0700

    use test recorder

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 go get github.com/gin-gonic/gin\
-go get github.com/stretchr/testify
+go get github.com/stretchr/testify\
+go get github.com/rs/xid
 
 go get -u
 
 curl localhost:8080
 
 go test
+
+GIN_MODE=release go test -v
--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
@@ -25,4 +27,23 @@ func TestIndexHandler(t *testing.T) {
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	responseData, _ := io.ReadAll(resp.Body)
 	assert.Equal(t, mockUserResp, string(responseData))
+}
+
+func SetupRouter() *gin.Engine {
+
+	router := gin.Default()
+	return router
+}
+
+func TestListRecipesHandler(t *testing.T) {
+
+	r := SetupRouter()
+	r.GET("/recipes", ListRecipesHandler)
+	req, _ := http.NewRequest("GET", "/recipes", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	var recipes []Recipe
+	json.Unmarshal([]byte(w.Body.String()), &recipes)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, 492, len(recipes))
 }
